8a8f8aac1896626d2507fdefe9b6878be7bf8ea8
Alexandr Vlasov <avlasov@akamai.com>
2023-08-07 10:26:36 +0200

    Bash prompt

--- bash-prompt.sh
+++ bash-prompt.sh
@@ -9,5 +9,5 @@ __myprompt() {
   ret=0
   (( $exit )) && ret=31
   branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-  PS1="$(__c 34)>>> $(__c $ret)R=$exit$(__c 0) [$(date +%H:%M)] \u@\h:$(__c 32)\w$(__c 33)${branch:+ ⎇ $branch}$(__c 0)${VIRTUAL_ENV:+ ⧉ ${VIRTUAL_ENV##*/}}\n\$ "
+  PS1="$(__c 34)┌ $(__c $ret)R=$exit$(__c 0) [$(date +%H:%M)] \u@\h:$(__c 32)\w$(__c 33)${branch:+ ⎇ $branch}$(__c 0)${VIRTUAL_ENV:+ ⧉ ${VIRTUAL_ENV##*/}}\n$(__c 34)└$(__c 0) \$ "
 }
