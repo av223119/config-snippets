8e8d1a01acab59b759f283c247beb6f8290b2de8
Alexandr Vlasov <alexander.vlasov@gmail.com>
2023-12-14 23:16:17 +0100

    treesitter for neovim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -32,4 +32,35 @@ require("lazy").setup({
 			vim.cmd.colorscheme "nord"
 		end
 	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"python",
+					"bash",
+					"markdown",
+					"html",
+					"htmldjango",
+					"dockerfile",
+					"ini",
+					"toml"
+				},
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false
+				},
+				indent = {
+					enable = true
+				}
+			})
+		end
+	},
 })
