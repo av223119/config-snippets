85b867def086ed7ab57641781cfc7d77863d6e69
Alexandr Vlasov <alexander.vlasov@gmail.com>
2023-12-16 21:54:20 +0100

    Change window navigation keymap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -9,6 +9,7 @@ function pum()
 	return vim.fn.pumvisible() == 1
 end
 
+--[[
 function quickfix()
 	for w = 1, vim.fn.winnr('$') do
 		if vim.fn.win_gettype(w) == "quickfix" then
@@ -17,14 +18,21 @@ function quickfix()
 	end
 	return false
 end
+vim.keymap.set({ "n", "i" }, "<C-up>", has(quickfix, "<cmd>cprev<CR>", nil), { expr = true })
+vim.keymap.set({ "n", "i" }, "<C-down>", has(quickfix, "<cmd>cnext<CR>", nil), { expr = true })
+--]]
 
-vim.keymap.set("c", "<down>", has(pum, "<c-n>", "<down>"), { expr = true })
-vim.keymap.set("c", "<up>", has(pum, "<c-p>", "<up>"), { expr = true })
-vim.keymap.set("c", "<left>", has(pum, "<up>", "<left>"), { expr = true })
-vim.keymap.set("c", "<right>", has(pum, "<down>", "<right>"), { expr = true })
+vim.keymap.set("c", "<Up>", has(pum, "<C-p>", "<Up>"), { expr = true })
+vim.keymap.set("c", "<Down>", has(pum, "<C-n>", "<Down>"), { expr = true })
+vim.keymap.set("c", "<Left>", has(pum, "<Up>", "<Left>"), { expr = true })
+vim.keymap.set("c", "<Right>", has(pum, "<Down>", "<Right>"), { expr = true })
 
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
-vim.keymap.set({ "n", "i" }, "<C-up>", has(quickfix, "<cmd>cprev<CR>", nil), { expr = true })
-vim.keymap.set({ "n", "i" }, "<C-down>", has(quickfix, "<cmd>cnext<CR>", nil), { expr = true })
+vim.keymap.set({ "n", "i" }, "<C-Up>", "<C-w><Up>")
+vim.keymap.set({ "n", "i" }, "<C-Down>", "<C-w><Down>")
+vim.keymap.set({ "n", "i" }, "<C-Left>", "<C-w><Left>")
+vim.keymap.set({ "n", "i" }, "<C-Right>", "<C-w><Right>")
+
+-- vim.keymap.set("n", "<Esc>", "<C-l>", { remap = true })
