c8871a88461f04015bc8b46168c4469731ecd7c6
Alexandr Vlasov <alexander.vlasov@gmail.com>
2023-12-16 15:48:22 +0100

    Keymaps for cprev/cnext

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,13 +1,30 @@
 -- vim.keymap.set with expr=true wants a function() object to call
-function ifpum(pos, neg)
+function has(cond, pos, neg)
 	return function()
-		return vim.fn.pumvisible() == 1 and pos or neg
+		return cond() and pos or neg
 	end
 end
-vim.keymap.set("c", "<down>", ifpum("<c-n>", "<down>"), { expr = true })
-vim.keymap.set("c", "<up>", ifpum("<c-p>", "<up>"), { expr = true })
-vim.keymap.set("c", "<left>", ifpum("<up>", "<left>"), { expr = true })
-vim.keymap.set("c", "<right>", ifpum("<down>", "<right>"), { expr = true })
+
+function pum()
+	return vim.fn.pumvisible() == 1
+end
+
+function quickfix()
+	for w = 1, vim.fn.winnr('$') do
+		if vim.fn.win_gettype(w) == "quickfix" then
+			return true
+		end
+	end
+	return false
+end
+
+vim.keymap.set("c", "<down>", has(pum, "<c-n>", "<down>"), { expr = true })
+vim.keymap.set("c", "<up>", has(pum, "<c-p>", "<up>"), { expr = true })
+vim.keymap.set("c", "<left>", has(pum, "<up>", "<left>"), { expr = true })
+vim.keymap.set("c", "<right>", has(pum, "<down>", "<right>"), { expr = true })
 
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
+
+vim.keymap.set({ "n", "i" }, "<C-up>", has(quickfix, "<cmd>cprev<CR>", nil), { expr = true })
+vim.keymap.set({ "n", "i" }, "<C-down>", has(quickfix, "<cmd>cnext<CR>", nil), { expr = true })
