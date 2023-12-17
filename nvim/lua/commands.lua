2887265ad9d5d83e12a5e11a66039b117a1d33fb
Alexandr Vlasov <alexander.vlasov@gmail.com>
2023-12-17 23:19:03 +0100

    slight improvement of the Grep command

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,12 +1,13 @@
 vim.api.nvim_create_user_command(
 	"Grep",
 	function(opts)
-		local r
-		r = vim.fn.systemlist { "git", "--no-pager", "grep", "-n", opts.fargs[1] }
+		local t = "git grep"
+		local r = vim.fn.systemlist { "git", "--no-pager", "grep", "-n", opts.fargs[1] }
 		if vim.v.shell_error == 128 then
+			t = "grep"
 			r = vim.fn.systemlist { "grep", "-R", "-n", opts.fargs[1], "." }
 		end
-		vim.fn.setqflist({}, ' ', { lines = r, title = opts.fargs[1] })
+		vim.fn.setqflist({}, ' ', { lines = r, title = t .. " " .. opts.fargs[1] })
 		vim.cmd.cwindow {}
 	end,
 	{ nargs = 1 }
