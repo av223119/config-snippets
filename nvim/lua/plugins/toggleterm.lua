058533694c5021c20a2d25606ce67c4cffb282b9
Alexandr Vlasov <avlasov@akamai.com>
2023-12-24 21:12:14 +0100

    minor toggleterm simplification

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,9 @@
 return {
 	"akinsho/toggleterm.nvim",
-	config = function()
-		require("toggleterm").setup({
-			autochdir = true,
-			direction = "float",
-			open_mapping = "<C-\\>",
-			start_in_insert = true,
-		})
-	end,
+	opts = {
+		autochdir = true,
+		direction = "float",
+		open_mapping = "<C-\\>",
+		start_in_insert = true,
+	},
 }
