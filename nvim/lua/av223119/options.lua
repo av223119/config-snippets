3ed7f41c1e1576b302e18cc11116fda4b5eb84d4
Alexandr Vlasov <avlasov@akamai.com>
2024-01-03 11:26:20 +0100

    set completeopt and clipboard to something saner

--- nvim/lua/av223119/options.lua
+++ nvim/lua/av223119/options.lua
@@ -1,14 +1,16 @@
-vim.opt.termguicolors = true
-vim.opt.number = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.scrolloff = 4
-vim.opt.virtualedit = "block"
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.path = { ".", "**" }
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = { "menu", "preview", "noselect" }
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
+vim.opt.ignorecase = true
+vim.opt.number = true
+vim.opt.path = { ".", "**" }
+vim.opt.scrolloff = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartcase = true
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.virtualedit = "block"
 
 -- Ideas
 -- vim.opt.inccommand
