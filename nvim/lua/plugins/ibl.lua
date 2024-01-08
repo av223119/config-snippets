9473f4a5466068dcc7343737ecb6b3fd4abde1d9
Alexandr Vlasov <avlasov@akamai.com>
2024-01-08 11:15:47 +0100

    ibl shouldn't highlight scope

--- nvim/lua/plugins/ibl.lua
+++ nvim/lua/plugins/ibl.lua
@@ -1,15 +1,14 @@
 return {
---[[
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
 	opts = {
 		indent = {
-			char = "¦", -- alternatives: BB¦ 4!┊
+			char = "│", -- alternatives: BB¦ 4!┊ vv│
 		},
 		scope = {
+			enabled = false,
 			show_start = false,
 			show_end = false,
 		},
 	},
---]]
 }
