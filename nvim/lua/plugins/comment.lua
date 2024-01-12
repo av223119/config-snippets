50f53ecf9aacec3cd157b0236aa8deca72580702
Alexandr Vlasov <avlasov@akamai.com>
2024-01-12 12:05:59 +0100

    Add comment.nvim, remove treesitter-context

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,9 @@
+return {
+	"numToStr/Comment.nvim",
+	opts = {
+		mappings = {
+			extra = false,
+		},
+	},
+	lazy = false,
+}
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,6 @@ return {
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
-				"c",
 				"lua",
 				"vim",
 				"vimdoc",
