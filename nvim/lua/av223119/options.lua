756c5c30435a21ebe4181e74f6ac96df35deb973
Alexandr Vlasov <alexander.vlasov@gmail.com>
2024-07-14 15:29:05 +0200

    completeopt changed

--- nvim/lua/av223119/options.lua
+++ nvim/lua/av223119/options.lua
@@ -1,5 +1,5 @@
 vim.opt.clipboard = "unnamedplus"
-vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.completeopt = { "menuone" }
 vim.opt.cursorlineopt = "number"
 vim.opt.cursorline = true
 vim.opt.diffopt = { "internal", "filler", "closeoff", "algorithm:minimal" }
