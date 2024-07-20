8de6442b7a5c23285072d93dad8674a3806dc145
Alexandr Vlasov <alexander.vlasov@gmail.com>
2024-07-20 13:11:35 +0200

    remove Grep custom command, commented out for ages

--- nvim/lua/av223119/commands.lua
+++ nvim/lua/av223119/commands.lua
@@ -1,21 +1,3 @@
---[[
--- Replaced by telescope live_grep
-vim.api.nvim_create_user_command(
-	"Grep",
-	function(opts)
-		local t = "git grep"
-		local r = vim.fn.systemlist { "git", "--no-pager", "grep", "-n", opts.fargs[1] }
-		if vim.v.shell_error == 128 then
-			t = "grep"
-			r = vim.fn.systemlist { "grep", "-R", "-n", opts.fargs[1], "." }
-		end
-		vim.fn.setqflist({}, ' ', { lines = r, title = t .. " " .. opts.fargs[1] })
-		vim.cmd.cwindow {}
-	end,
-	{ nargs = 1 }
-)
-]]
-
 vim.api.nvim_create_augroup("__custom", {})
 
 vim.api.nvim_create_autocmd("FileType", {
