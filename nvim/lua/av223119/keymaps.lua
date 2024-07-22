05dd9e1ea8268646d8cea0a33401b5d4fe9e5294
Alexandr Vlasov <avlasov@akamai.com>
2024-07-22 17:00:49 +0200

    Add quickfix navigation

--- nvim/lua/av223119/keymaps.lua
+++ nvim/lua/av223119/keymaps.lua
@@ -9,8 +9,7 @@ local function pum()
 	return vim.fn.pumvisible() == 1
 end
 
---[[
-function quickfix()
+local function quickfix()
 	for w = 1, vim.fn.winnr("$") do
 		if vim.fn.win_gettype(w) == "quickfix" then
 			return true
@@ -18,9 +17,8 @@ function quickfix()
 	end
 	return false
 end
-vim.keymap.set({ "n", "i" }, "<C-up>", has(quickfix, "<cmd>cprev<CR>", nil), { expr = true })
-vim.keymap.set({ "n", "i" }, "<C-down>", has(quickfix, "<cmd>cnext<CR>", nil), { expr = true })
---]]
+vim.keymap.set({ "n", "i" }, "[q", has(quickfix, "<cmd>cprev<CR>", nil), { expr = true })
+vim.keymap.set({ "n", "i" }, "]q", has(quickfix, "<cmd>cnext<CR>", nil), { expr = true })
 
 vim.keymap.set("c", "<Up>", has(pum, "<C-p>", "<Up>"), { expr = true })
 vim.keymap.set("c", "<Down>", has(pum, "<C-n>", "<Down>"), { expr = true })
