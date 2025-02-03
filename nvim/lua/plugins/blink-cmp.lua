f1fdabffdc349bfae441964c3537c4e54b84f916
Alexandr Vlasov <avlasov@akamai.com>
2025-02-03 18:14:05 +0100

    Update blink-cmp to 0.11.0

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -2,7 +2,7 @@ return {
 	"saghen/blink.cmp",
 	-- optional: provides snippets for the snippet source
 	-- dependencies = "rafamadriz/friendly-snippets",
-	version = "v0.10.0",
+	version = "v0.11.0",
 	opts = {
 		keymap = {
 			preset = "super-tab",
