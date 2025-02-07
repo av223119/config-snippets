6e850d11df1b8ae84e1e2cbbca26d1efe939edaf
Alexandr Vlasov <avlasov@akamai.com>
2025-02-07 20:58:41 +0100

    Small simplification of the prompt

--- bash/99-prompt.sh
+++ bash/99-prompt.sh
@@ -9,6 +9,5 @@ _myprompt() {
   ret=0
   (( $exit )) && ret=31
   branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-  title="\[\e]0;\u@\h:\w\a\]"
-  PS1="$title$(_c 34)┌ $(_c $ret)R=$exit$(_c 0) [$(date +%H:%M)] \u@\h:$(_c 32)\w$(_c 33)${branch:+ ⎇ $branch}$(_c 0)${VIRTUAL_ENV:+ ⧉ ${VIRTUAL_ENV##*/}}\n$(_c 34)└$(_c 0) \$ "
+  PS1="\[\e]0;\u@\h:\w\a\]$(_c 34)┌ $(_c $ret)R=$exit$(_c 0) [\A] \u@\h:$(_c 32)\w$(_c 33)${branch:+ ⎇ $branch}$(_c 0)${VIRTUAL_ENV:+ ⧉ ${VIRTUAL_ENV##*/}}\n$(_c 34)└$(_c 0) \$ "
 }
