e31ae08fd43af5481582927440ee023b6713c747
Alexandr Vlasov <avlasov@akamai.com>
2025-02-13 13:21:37 +0100

    Make keys a function in snacks

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -8,32 +8,38 @@ return {
 			scope = { enabled = false },
 		},
 		terminal = {},
-		picker = { layout = "bottom" },
-		input = {},
-	},
-	keys = {
-		{ "<C-\\>",     function() Snacks.terminal.toggle() end, mode = { "n", "t" }, desc = "Toggle terminal" },
-		{ "<leader>ld", function() Snacks.picker.lsp_definitions() end, desc = "LSP: definition" },
-		{ "<leader>lr", function() Snacks.picker.lsp_references() end, desc = "LSP: references" },
-		{ "<leader>ls", function() Snacks.picker.lsp_symbols { layout = "vscode" } end, desc = "LSP: symbols" },
-		{ "<leader>fS", function() Snacks.picker.pickers { layout = "vscode" } end, desc = "Find: pickers" },
-		{ "<leader>fb", function() Snacks.picker.buffers() end, desc = "Find: buffers" },
-		{ "<leader>fo", function() Snacks.picker.recent() end, desc = "Find: old files" },
-		{
-			"<leader>fg",
-			function()
-				return Snacks.git.get_root() and Snacks.picker.git_grep() or Snacks.picker.grep()
-			end,
-			desc = "Find: grep"
+		picker = {
+			layout = "bottom",
+			prompt = "›",
 		},
-		{
-			"<leader>ff",
-			function()
-				return Snacks.git.get_root() and Snacks.picker.git_files() or Snacks.picker.files()
-			end,
-			desc = "Find: files"
-		},
-		{ "<leader>fG", function() Snacks.picker.grep() end, desc = "Find: grep all" },
-		{ "<leader>fF", function() Snacks.picker.files() end, desc = "Find: files all" },
+		input = { icon = "›" },
 	},
+	keys = function()
+		local s = require("snacks")
+		return {
+			{ "<C-\\>",     function() s.terminal.toggle() end, mode = { "n", "t" }, desc = "Toggle terminal" },
+			{ "<leader>ld", function() s.picker.lsp_definitions() end, desc = "LSP: definition" },
+			{ "<leader>lr", function() s.picker.lsp_references() end, desc = "LSP: references" },
+			{ "<leader>ls", function() s.picker.lsp_symbols { layout = "vscode" } end, desc = "LSP: symbols" },
+			{ "<leader>fS", function() s.picker.pickers { layout = "vscode" } end, desc = "Find: pickers" },
+			{ "<leader>fb", function() s.picker.buffers() end, desc = "Find: buffers" },
+			{ "<leader>fo", function() s.picker.recent() end, desc = "Find: old files" },
+			{
+				"<leader>fg",
+				function()
+					return s.git.get_root() and s.picker.git_grep() or s.picker.grep()
+				end,
+				desc = "Find: grep"
+			},
+			{
+				"<leader>ff",
+				function()
+					return s.git.get_root() and s.picker.git_files() or s.picker.files()
+				end,
+				desc = "Find: files"
+			},
+			{ "<leader>fG", function() s.picker.grep() end, desc = "Find: grep all" },
+			{ "<leader>fF", function() s.picker.files() end, desc = "Find: files all" },
+		}
+	end
 }
