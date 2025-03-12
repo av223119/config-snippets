1386d7c3614e7f4eee04386669f247055b1311a7
Alexandr Vlasov <avlasov@akamai.com>
2025-03-12 12:13:26 +0100

    Revert nord to default settings; good enough

--- nvim/lua/plugins/nord.lua
+++ nvim/lua/plugins/nord.lua
@@ -1,8 +1,8 @@
 return {
 	"gbprod/nord.nvim",
 	opts = {
-		on_highlights = function(hightlights, _)
-			hightlights.Normal.bg = "#20303C"
-		end,
+		-- on_highlights = function(hightlights, _)
+		-- 	hightlights.Normal.bg = "#20303C"
+		-- end,
 	},
 }
