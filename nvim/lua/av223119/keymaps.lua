0ca1be13c773aab2169f5e72016da216e60488b6
Alexandr Vlasov <alexander.vlasov@gmail.com>
2025-03-27 21:40:56 +0100

    Remove unused keymaps

--- nvim/lua/av223119/keymaps.lua
+++ nvim/lua/av223119/keymaps.lua
@@ -23,10 +23,6 @@ vim.keymap.set({ "n", "i" }, "]q", has(quickfix, "<cmd>cnext<CR>", nil), { expr 
 
 vim.keymap.set("c", "<Up>", has(pum, "<C-p>", "<Up>"), { expr = true })
 vim.keymap.set("c", "<Down>", has(pum, "<C-n>", "<Down>"), { expr = true })
-vim.keymap.set("c", "<Left>", has(pum, "<Up>", "<Left>"), { expr = true })
-vim.keymap.set("c", "<Right>", has(pum, "<Down>", "<Right>"), { expr = true })
 
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
-
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
