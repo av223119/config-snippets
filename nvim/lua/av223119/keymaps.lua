f32786bf3230460dda112bf146f9ec72bd683e7b
Alexandr Vlasov <alexander.vlasov@gmail.com>
2025-03-27 21:42:47 +0100

    [q and ]q are mapped by default since nvim 0.11

--- nvim/lua/av223119/keymaps.lua
+++ nvim/lua/av223119/keymaps.lua
@@ -9,18 +9,6 @@ local function pum()
 	return vim.fn.pumvisible() == 1
 end
 
-local function quickfix()
-	for w = 1, vim.fn.winnr("$") do
-		if vim.fn.win_gettype(w) == "quickfix" then
-			return true
-		end
-	end
-	return false
-end
-
-vim.keymap.set({ "n", "i" }, "[q", has(quickfix, "<cmd>cprev<CR>", nil), { expr = true })
-vim.keymap.set({ "n", "i" }, "]q", has(quickfix, "<cmd>cnext<CR>", nil), { expr = true })
-
 vim.keymap.set("c", "<Up>", has(pum, "<C-p>", "<Up>"), { expr = true })
 vim.keymap.set("c", "<Down>", has(pum, "<C-n>", "<Down>"), { expr = true })
 
