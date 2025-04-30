8259f6155bcf56993853a11f80b270961947cf6c
Alexandr Vlasov <alexander.vlasov@gmail.com>
2025-04-30 18:06:23 +0200

    Make LSP keymaps unconditional, it doesn't hurt

--- nvim/lua/av223119/keymaps.lua
+++ nvim/lua/av223119/keymaps.lua
@@ -14,3 +14,9 @@ vim.keymap.set("c", "<Down>", has(pum, "<C-n>", "<Down>"), { expr = true })
 
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
+
+vim.keymap.set("n", "<leader>lR", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", "<leader>lC", vim.lsp.buf.code_action, { desc = "Code_action" })
+vim.keymap.set("n", "<leader>lF", vim.lsp.buf.format, { desc = "Format" })
+vim.keymap.set("n", "<leader>lh", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end,
+	{ desc = "togge inlay hints" })
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -11,38 +11,5 @@ return {
 				vim.lsp.enable(name)
 			end,
 		}
-
-		local lsp_keys = {
-			{ key = "<leader>lR", func = vim.lsp.buf.rename,           desc = "Rename" },
-			{ key = "<leader>lC", func = vim.lsp.buf.code_action,      desc = "Code_action" },
-			{ key = "<leader>lF", func = vim.lsp.buf.format,           desc = "Format" },
-			{
-				key = "<leader>lh",
-				func = function()
-					vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-				end,
-				desc = "togge inlay hints"
-			},
-		}
-
-		vim.api.nvim_create_augroup("lsp_group", {})
-
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = "lsp_group",
-			callback = function(ev)
-				for _, v in ipairs(lsp_keys) do
-					vim.keymap.set("n", v.key, v.func, { buffer = ev.buf, desc = "LSP: " .. v.desc })
-				end
-			end,
-		})
-
-		vim.api.nvim_create_autocmd("LspDetach", {
-			group = "lsp_group",
-			callback = function(ev)
-				for _, v in ipairs(lsp_keys) do
-					vim.keymap.del("n", v.key, { buffer = ev.buf })
-				end
-			end,
-		})
 	end,
 }
