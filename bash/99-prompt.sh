5dd421d262495eecb64202bcdb5ac2ae510e7890
Alexandr Vlasov <avlasov@akamai.com>
2026-01-28 19:16:30 +0100

    Use `git branch --show-current`

--- bash/99-prompt.sh
+++ bash/99-prompt.sh
@@ -8,6 +8,6 @@ _myprompt() {
   local exit="$?"
   ret=0
   (( $exit )) && ret=31
-  branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+  branch=$(git branch --show-current 2>/dev/null)
   PS1="\[\e]0;\u@\h:\w\a\]$(_c 34)┌ $(_c $ret)R=$exit$(_c 0) [\A] \u@\h:$(_c 32)\w$(_c 33)${branch:+ ⎇ $branch}$(_c 0)${VIRTUAL_ENV:+ ⧉ ${VIRTUAL_ENV##*/}}\n$(_c 34)└$(_c 0) \$ "
 }
